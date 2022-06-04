e6582f8137802d779801a8125f43950f222cda61
Omer Anwar <omeranwar552@gmail.com>
2022-06-04 08:48:21 -0700

    process storing

--- python/main.py
+++ python/main.py
@@ -5,13 +5,18 @@ from util import extract_suspicious
 
 if __name__ == "__main__":
 
+    sus_procs = {}
     sus_processes = extract_suspicious()
-
+    # print(sus_processes)
     print("Suspicous processes:")
     for process in sus_processes:
-        print("=============")
-        print(process["pid"])
-        print(process["name"])
-        print(process["cpu_usage"])
-        print(process["create_time"])
-        print("=============\n")
+        if process["pid"] not in sus_procs:
+            print("=============")
+            print(process["pid"])
+            print(process["name"])
+            print(process["cpu_usage"])
+            print(process["create_time"])
+            print("=============\n")
+            sus_procs[process["pid"]] = process["cpu_usage"]
+
+    print(sus_procs)
