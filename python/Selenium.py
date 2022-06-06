238514f8cd6d424a7e8c7b829f873aae50aa8507
Adan Abbas <adanabbas7950@gmail.com>
2022-06-06 17:48:46 +0500

    Updated Selenuim

--- python/Selenium.py
+++ python/Selenium.py
@@ -1,39 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
 import urllib.request
 
+# Returns in key value paairs the javascript on a given web page
+def get_Javascript(url):
+    chromedriver_autoinstaller.install()
+    # WEBDRIVER_PATH = 'C:\Windows\chromedriver'
+    driver = webdriver.Chrome()
+    URL = url
+    driver.get(URL)
+    scripts = driver.find_elements(By.TAG_NAME, "script")
 
-# def get_url(url):
-#     WEBDRIVER_PATH = 'C:\Windows\chromedriver'
-#     driver = webdriver.Chrome(WEBDRIVER_PATH)
-#     # URL = url
-#     # driver.get(URL)
-#     # scripts = driver.find_elements(By.TAG_NAME, "script")
+    sources = {}
+    sources["Embedded JS"] = ""
+    for script in scripts:
+        sources[script.get_attribute("src")] = ""
 
-#     # sources = {}
-#     # for script in scripts:
-#     #     sources[script.get_attribute("src")] = ""
+        # check for embedded javascript
+        if script.get_attribute("innerHTML") != "":
+            sources["Embedded JS"] += script.get_attribute("innerHTML")
+            
 
-#     # for source in sources.keys():
-#     #     url = source
-#     #     file = urllib.request.urlopen(url)
-#     #     sources[source] = file.read().decode('utf-8')
-#     #     # print(sources[source])
-#     #     # print()
-#     # driver.quit()
-#     # return sources
-#     return []
+    for source in sources.keys():
+        url = source
+        if url == '' or url == "Embedded JS":
+            continue
+        try:
+            response = urllib.request.urlopen(url)
+            sources[source] = response.read().decode('utf-8')
+        except:
+            print("Error: " + url)
+            continue
 
-
-# values = get_url(
-#     "http://127.0.0.1:3000/miner/index.html")
-# for value in values.keys():
-#     print(value)
-#     # print()
-
-rules = yara.compile(filepath='./rules.yara')
-a = "This is my malicious string hash"
-
-matches = rules.match(data = a)
-
-print(matches)
+    driver.quit()
+    return sources
