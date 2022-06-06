238390d78bf6aab2465f390d8a9f68741940597d
Adan Abbas <adanabbas7950@gmail.com>
2022-06-06 17:24:21 +0500

    Moved Yara implementation to own file

--- python/Selenium.py
+++ python/Selenium.py
@@ -1,32 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib.request
-# import yara
 
 
-def get_url(url):
-    WEBDRIVER_PATH = '/usr/local/bin/chromedriver'
-    driver = webdriver.Chrome(WEBDRIVER_PATH)
-    URL = url
-    driver.get(URL)
-    scripts = driver.find_elements(By.TAG_NAME, "script")
+# def get_url(url):
+#     WEBDRIVER_PATH = 'C:\Windows\chromedriver'
+#     driver = webdriver.Chrome(WEBDRIVER_PATH)
+#     # URL = url
+#     # driver.get(URL)
+#     # scripts = driver.find_elements(By.TAG_NAME, "script")
 
-    sources = {}
-    for script in scripts:
-        sources[script.get_attribute("src")] = ""
+#     # sources = {}
+#     # for script in scripts:
+#     #     sources[script.get_attribute("src")] = ""
 
-    for source in sources.keys():
-        url = source
-        file = urllib.request.urlopen(url)
-        sources[source] = file.read().decode('utf-8')
-        # print(sources[source])
-        # print()
-    driver.quit()
-    return sources
+#     # for source in sources.keys():
+#     #     url = source
+#     #     file = urllib.request.urlopen(url)
+#     #     sources[source] = file.read().decode('utf-8')
+#     #     # print(sources[source])
+#     #     # print()
+#     # driver.quit()
+#     # return sources
+#     return []
 
 
-values = get_url(
-    "http://127.0.0.1:3000/miner/index.html")
-for value in values.keys():
-    print(value)
-    # print()
+# values = get_url(
+#     "http://127.0.0.1:3000/miner/index.html")
+# for value in values.keys():
+#     print(value)
+#     # print()
+
+rules = yara.compile(filepath='./rules.yara')
+a = "This is my malicious string hash"
+
+matches = rules.match(data = a)
+
+print(matches)
